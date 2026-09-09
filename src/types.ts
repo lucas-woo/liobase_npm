a6ec6b6ed94e107569542d9db079d2c5a9cab546
lucas <lucaswoo583@gmail.com>
2026-09-09 03:28:32 -0600

    object upload request types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,17 @@
 export interface ClientOptions {
-
   apiKey: string;
-
   apiSecret: string;
-
   baseUrl?: string;
+}
+
+export interface UploadObjectMetadata {
+  projectId: string;
+  name: string;
+  originalFileName: string;
+  folderId: string;
+  isActive: boolean;
+}
+
+export interface UploadObjectResponse {
+  objectId: string;
 }
