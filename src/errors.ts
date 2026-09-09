0b787bb17168cf8a5be2a3d67fa8ee2356847e05
lucas <lucaswoo583@gmail.com>
2026-09-09 02:44:45 -0600

    error types

---
+++ src/errors.ts
@@ -0,0 +1,11 @@
+export class ApiError extends Error {
+  public status: number;
+  public data?: unknown;
+
+  constructor(status: number, message: string, data?: unknown) {
+    super(message);
+    this.name = 'ApiError';
+    this.status = status;
+    this.data = data;
+  }
+}
